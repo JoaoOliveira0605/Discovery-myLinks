ab261cf9297ab82beafbb93db165ea6351b6f9b5
JOAO-VITOR <joaooliveira2000.dev@gmail.com>
2023-06-21 09:07:44 -0300

    Added different profile pic for light mode and different alt

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -1,11 +1,25 @@
 function toggleMode() {
   const html = document.documentElement
+  html.classList.toggle('light')
 
-  // if (html.classList.contains("light")) {
-  //   html.classList.remove("light")
-  // } else {
-  //   html.classList.add("light")
-  // }
+  // pegar tag da img
+  const img = document.querySelector('#profile img')
 
-  html.classList.toggle("light")
+  // Substituir a img
+
+  if (html.classList.contains('light')) {
+    // Se tiver light mode, adicionar imagem light
+    img.setAttribute('src', './assets/Profile/light-pic.jpg')
+    img.setAttribute(
+      'alt',
+      'Foto em preto e branco de Joao Vitor segurando seu celular e sorrindo, usando uma camisa social em frente ao espelho'
+    )
+  } else {
+    // Se tiver sem light mode, manter imagem normal
+    img.setAttribute('src', './assets/Profile/picture.jpg')
+    img.setAttribute(
+      'alt',
+      'Foto de Joao Vitor segurando seu celular e sorrindo, usando uma blusa azul em frente ao espelho'
+    )
+  }
 }
